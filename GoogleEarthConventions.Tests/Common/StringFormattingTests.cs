686928d556c9862e991ce07df8d277c73b311fc8
Ben Penaluna <penalunaben@gmail.com>
2020-12-23 15:29:30 +1100

    Created an extension method to correctly format properties to KML format

---
+++ GoogleEarthConventions.Tests/Common/StringFormattingTests.cs
@@ -0,0 +1,25 @@
+﻿using GoogleEarthConversions.Core.Common;
+using Xunit;
+
+namespace GoogleEarthConventions.Tests.Common
+{
+    public class StringFormattingTests
+    {
+        [Theory]
+        [InlineData("","")]
+        [InlineData("0","0")]
+        [InlineData("A","a")]
+        [InlineData("a","a")]
+        [InlineData("Test", "test")]
+        [InlineData("TEST", "tEST")]
+        [InlineData("Test00", "test00")]
+        [InlineData("0Test", "0Test")]
+        [InlineData(":Test", ":Test")]
+        public void StringFormatting_ConvertFirstCharacterToLowerCase(string testString, string expected)
+        {
+            var result = testString.ConvertFirstCharacterToLowerCase();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ GoogleEarthConversions/Common/StringFormatting.cs
@@ -0,0 +1,22 @@
+﻿namespace GoogleEarthConversions.Core.Common
+{
+    public static class StringFormatting
+    {
+        public static string ConvertFirstCharacterToLowerCase(this string stringToConvert)
+        {
+            var convertedString = "";
+
+            if (stringToConvert.Length == 1)
+                return stringToConvert.ToLower();
+
+            if (stringToConvert.Length > 1)
+            {
+                var firstCharacter = stringToConvert.Substring(0, 1);
+                var stringRemainder = stringToConvert.Substring(1, stringToConvert.Length - 1);
+                return firstCharacter.ToLower() + stringRemainder;
+            }
+
+            return convertedString;
+        }
+    }
+}
