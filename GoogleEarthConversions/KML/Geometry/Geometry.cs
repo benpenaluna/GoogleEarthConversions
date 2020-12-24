2aa763cef9390db0c0db63eaa436eed21412f7dd
Ben Penaluna <penalunaben@gmail.com>
2020-12-24 20:52:16 +1100

    Moved Extrude and AltitudeMode to Geometry in order to allow these properties to be used by all class derived from Geometry

--- GoogleEarthConversions/KML/Geometry/Geometry.cs
+++ GoogleEarthConversions/KML/Geometry/Geometry.cs
@@ -5,6 +5,61 @@ namespace GoogleEarthConversions.Core.KML.Geometry
 {
     public abstract class Geometry : GoogleEarthObject, IKMLFormat
     {
+        protected IAltitudeMode _altitudeMode;
+        public IAltitudeMode AltitudeMode
+        {
+            get { return _altitudeMode; }
+            set
+            {
+                _altitudeMode = value;
+                AltitudeMode.AltMode_Changed += AltMode_OnChanged;
+            }
+        }
+
+        private void AltMode_OnChanged(object sender, EventArgs e)
+        {
+            if (AltitudeMode.AltMode == AltMode.ClampToGround || AltitudeMode.AltMode == AltMode.ClampToSeaFloor)
+                _extrude.Extruded = false;
+        }
+
+        private IExtrude _extrude;
+        public IExtrude Extrude
+        {
+            get { return _extrude; }
+            set
+            {
+                if (value.Extruded == false || (_altitudeMode.AltMode != AltMode.ClampToGround && _altitudeMode.AltMode != AltMode.ClampToSeaFloor))
+                {
+                    _extrude = value;
+                    Extrude.Extruded_Changed += Extrude_OnChanged;
+                }
+                else
+                    ThrowInvalidOperationExceptionOnExtrude();
+            }
+        }
+
+        private void Extrude_OnChanged(object sender, EventArgs e)
+        {
+            if (Extrude.Extruded == false)
+                return;
+
+            if (AltitudeMode.AltMode == AltMode.ClampToGround || AltitudeMode.AltMode == AltMode.ClampToSeaFloor)
+            {
+                Extrude.Extruded = false;
+                ThrowInvalidOperationExceptionOnExtrude();
+            }
+        }
+
+        private void ThrowInvalidOperationExceptionOnExtrude()
+        {
+            var message = string.Format("Property '{0}' cannot be set to true when '{1}' is set to '{2}' or '{3}'.",
+                                                            nameof(Extrude.Extruded),
+                                                            nameof(AltitudeMode.AltMode),
+                                                            nameof(AltMode.ClampToGround),
+                                                            nameof(AltMode.ClampToSeaFloor));
+            throw new InvalidOperationException(message);
+        }
+
         public abstract string ConvertObjectToKML();
 
         protected string OpeningTag(Type type)
--- GoogleEarthConversions/KML/Geometry/Point.cs
+++ GoogleEarthConversions/KML/Geometry/Point.cs
@@ -11,33 +11,6 @@ namespace GoogleEarthConversions.Core.KML.Geometry
 
         public ICoordinates Coordinates { get; set; }
 
-        private IAltitudeMode _altitudeMode;
-        public IAltitudeMode AltitudeMode
-        {
-            get { return _altitudeMode; }
-            set 
-            { 
-                _altitudeMode = value;
-                AltitudeMode.AltMode_Changed += AltMode_OnChanged;
-            }
-        }
-                
-        private IExtrude _extrude;
-        public IExtrude Extrude
-        {
-            get { return _extrude; }
-            set 
-            {
-                if (value.Extruded == false || (_altitudeMode.AltMode != AltMode.ClampToGround && _altitudeMode.AltMode != AltMode.ClampToSeaFloor))
-                {
-                    _extrude = value;
-                    Extrude.Extruded_Changed += Extrude_OnChanged;
-                }
-                else
-                    ThrowInvalidOperationExceptionOnExtrude();
-            }
-        }
-
         public Point(string id)
         {
             InitialiseProperties(id, new Coordinates());
@@ -54,34 +27,6 @@ namespace GoogleEarthConversions.Core.KML.Geometry
             Extrude = new Extrude();
             AltitudeMode = new AltitudeMode();
             Coordinates = coordinate;
-        }
-
-        private void AltMode_OnChanged(object sender, EventArgs e)
-        {
-            if (AltitudeMode.AltMode == AltMode.ClampToGround || AltitudeMode.AltMode == AltMode.ClampToSeaFloor)
-                _extrude.Extruded = false;
-        }
-
-        private void Extrude_OnChanged(object sender, EventArgs e)
-        {
-            if (Extrude.Extruded == false)
-                return;
-            
-            if (AltitudeMode.AltMode == AltMode.ClampToGround || AltitudeMode.AltMode == AltMode.ClampToSeaFloor)
-            {
-                Extrude.Extruded = false;
-                ThrowInvalidOperationExceptionOnExtrude();
-            }
-        }
-
-        private void ThrowInvalidOperationExceptionOnExtrude()
-        {
-            var message = string.Format("Property '{0}' cannot be set to true when '{1}' is set to '{2}' or '{3}'.",
-                                                            nameof(Extrude.Extruded),
-                                                            nameof(AltitudeMode.AltMode),
-                                                            nameof(AltMode.ClampToGround),
-                                                            nameof(AltMode.ClampToSeaFloor));
-            throw new InvalidOperationException(message);
         }
 
         public override bool Equals(object obj)
