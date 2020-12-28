2fe781f1126aeeecf0530dfe94e6cf47a17ab6e1
Ben Penaluna <penalunaben@gmail.com>
2020-12-28 21:45:49 +1100

    Completed the Roll class to be used as a property in the Camera class

---
+++ GoogleEarthConventions.Tests/KML/AbstractView/Attributes/RollTests.cs
@@ -0,0 +1,77 @@
+﻿using GeoFunctions.Core.Coordinates;
+using GeoFunctions.Core.Coordinates.Measurement;
+using GoogleEarthConversions.Core.KML.AbstractView.Attributes;
+using System;
+using Xunit;
+
+namespace GoogleEarthConventions.Tests.KML.AbstractView.Attributes
+{
+    public class RollTests
+    {
+        [Fact]
+        public void Roll_CanInstantiate()
+        {
+            var sut = new Roll();
+            Assert.NotNull(sut);
+        }
+
+        [Fact]
+        public void Roll_AllPropertiesInitialised()
+        {
+            var sut = new Roll();
+
+            foreach (var prop in sut.GetType().GetProperties())
+            {
+                var value = prop.GetValue(sut);
+                Assert.NotNull(value);
+            }
+        }
+
+        [Theory]
+        [InlineData(-180.0, AngleMeasurement.Degrees)]
+        [InlineData(0.0, AngleMeasurement.Degrees)]
+        [InlineData(180.0, AngleMeasurement.Degrees)]
+        [InlineData(-Math.PI, AngleMeasurement.Radians)]
+        [InlineData(0.0, AngleMeasurement.Radians)]
+        [InlineData(Math.PI, AngleMeasurement.Radians)]
+        public void Roll_CanInstantiateWithValidDegree(double angle, AngleMeasurement angleMeasurement)
+        {
+            IAngle testAngle = new Angle(angle, angleMeasurement);
+            var expected = testAngle;
+
+            var sut = new Roll(angle, angleMeasurement);
+            var result = sut.Angle;
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(double.MinValue, AngleMeasurement.Degrees)]
+        [InlineData(-180.0000000000001, AngleMeasurement.Degrees)]
+        [InlineData(180.0000000000001, AngleMeasurement.Degrees)]
+        [InlineData(-3.1415926535899, AngleMeasurement.Radians)]
+        [InlineData(3.1415926535899, AngleMeasurement.Radians)]
+        [InlineData(double.MaxValue, AngleMeasurement.Degrees)]
+        public void Roll_CannotInstantiateWithInvalidDegree(double angle, AngleMeasurement angleMeasurement)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Roll(angle, angleMeasurement));
+        }
+
+        [Theory]
+        [InlineData(-180.0, AngleMeasurement.Degrees, "<roll>-180</roll>")]
+        [InlineData(0.0, AngleMeasurement.Degrees, "<roll>0</roll>")]
+        [InlineData(180.0, AngleMeasurement.Degrees, "<roll>180</roll>")]
+        [InlineData(72.5366278, AngleMeasurement.Degrees, "<roll>72.5366278</roll>")]
+        [InlineData(-Math.PI, AngleMeasurement.Radians, "<roll>-180</roll>")]
+        [InlineData(0.0, AngleMeasurement.Radians, "<roll>0</roll>")]
+        [InlineData(Math.PI, AngleMeasurement.Radians, "<roll>180</roll>")]
+        public void Roll_CorrectlyConvertsToKML(double angle, AngleMeasurement angleMeasurement, string expected)
+        {
+            var sut = new Roll(angle, angleMeasurement);
+
+            var result = sut.ConvertObjectToKML();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ GoogleEarthConversions/KML/AbstractView/Attributes/Roll.cs
@@ -0,0 +1,64 @@
+﻿using GeoFunctions.Core.Coordinates;
+using GeoFunctions.Core.Coordinates.Measurement;
+using GeoFunctions.Core.Coordinates.Structs;
+using GoogleEarthConversions.Core.Common;
+using System;
+using System.Globalization;
+
+namespace GoogleEarthConversions.Core.KML.AbstractView.Attributes
+{
+    public class Roll : SphericalCoordinate, IKMLFormat
+    {
+        private IAngle _angle;
+
+        public sealed override IAngle Angle
+        {
+            get => _angle;
+            set
+            {
+                var maxValue = value.AngleMeasurement == AngleMeasurement.Degrees ? 180.0 : Math.PI;
+                if (Math.Abs(value.Value) > maxValue)
+                    throw new ArgumentOutOfRangeException(value.Value.ToString(CultureInfo.InvariantCulture));
+
+                _angle = value;
+            }
+        }
+
+        public override DmsCoordinate DmsCoordinate => CalculateDmsCoordinate(Angle.Value >= 0.0 ? Hemisphere.North : Hemisphere.South);
+
+        public Roll()
+        {
+            Angle = new Angle();
+        }
+
+        public Roll(double angle, AngleMeasurement measurement)
+        {
+            Angle = new Angle(angle, measurement);
+        }
+
+        public Roll(IAngle angle)
+        {
+            Angle = new Angle(angle.Value, angle.AngleMeasurement);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj.GetType() == typeof(Roll) && Equals((Roll)obj);
+        }
+
+        protected bool Equals(Roll other)
+        {
+            return Equals(Angle, other.Angle);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public string ConvertObjectToKML()
+        {
+            return string.Format("<{0}>{1}</{0}>", nameof(Roll).ConvertFirstCharacterToLowerCase(), Angle.ToDegrees());
+        }
+    }
+}
