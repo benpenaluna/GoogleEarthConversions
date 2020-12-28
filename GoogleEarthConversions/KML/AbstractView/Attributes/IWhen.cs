6a68b31c11764704f7171d50ff6418408db57e91
Ben Penaluna <penalunaben@gmail.com>
2020-12-28 13:37:20 +1100

    Completed When class for the upcoming TimeStamp class

---
+++ GoogleEarthConversions/KML/AbstractView/Attributes/IWhen.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GoogleEarthConversions.Core.KML.AbstractView.Attributes
+{
+    public interface IWhen : IKMLFormat
+    {
+        DateTime LocalDateTime { get; set; }
+        TimeZoneInfo TimeZone { get; set; }
+    }
+}
