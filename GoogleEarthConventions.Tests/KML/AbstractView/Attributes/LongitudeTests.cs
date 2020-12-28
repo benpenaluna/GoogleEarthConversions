a67de29ea36670a952ec58c0a55db5765e139c21
Ben Penaluna <penalunaben@gmail.com>
2020-12-28 21:58:23 +1100

    Extended GeoFunctions.Core.Coordinates.Longitude in order to produce the required KML

---
+++ GoogleEarthConventions.Tests/KML/AbstractView/Attributes/LongitudeTests.cs
@@ -0,0 +1,27 @@
+﻿using GeoFunctions.Core.Coordinates.Measurement;
+using GoogleEarthConversions.Core.KML.AbstractView.Attributes;
+using System;
+using Xunit;
+
+namespace GoogleEarthConventions.Tests.KML.AbstractView.Attributes
+{
+    public class LongitudeTests
+    {
+        [Theory]
+        [InlineData(-179.9999999, AngleMeasurement.Degrees, "<longitude>-179.9999999</longitude>")]
+        [InlineData(0.0, AngleMeasurement.Degrees, "<longitude>0</longitude>")]
+        [InlineData(180.0, AngleMeasurement.Degrees, "<longitude>180</longitude>")]
+        [InlineData(72.5366278, AngleMeasurement.Degrees, "<longitude>72.5366278</longitude>")]
+        [InlineData(-Math.PI + 0.0000000000001, AngleMeasurement.Radians, "<longitude>-179.9999999999943</longitude>")]
+        [InlineData(0.0, AngleMeasurement.Radians, "<longitude>0</longitude>")]
+        [InlineData(Math.PI, AngleMeasurement.Radians, "<longitude>180</longitude>")]
+        public void Longitude_CorrectlyConvertsToKML(double angle, AngleMeasurement angleMeasurement, string expected)
+        {
+            var sut = new Longitude(angle, angleMeasurement);
+
+            var result = sut.ConvertObjectToKML();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ GoogleEarthConversions/KML/AbstractView/Attributes/Longitude.cs
@@ -0,0 +1,23 @@
+﻿using GeoFunctions.Core.Coordinates;
+using GeoFunctions.Core.Coordinates.Measurement;
+using GoogleEarthConversions.Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleEarthConversions.Core.KML.AbstractView.Attributes
+{
+    public class Longitude : GeoFunctions.Core.Coordinates.Longitude, IKMLFormat
+    {
+        public Longitude() : base() { }
+
+        public Longitude(double angle, AngleMeasurement measurement = AngleMeasurement.Degrees) : base(angle, measurement) { }
+
+        public Longitude(IAngle angle) : base(angle) { }
+
+        public string ConvertObjectToKML()
+        {
+            return string.Format("<{0}>{1}</{0}>", nameof(Longitude).ConvertFirstCharacterToLowerCase(), Angle.ToDegrees());
+        }
+    }
+}
