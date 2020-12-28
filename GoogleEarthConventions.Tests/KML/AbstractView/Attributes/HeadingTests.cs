4049553f6effe95b60da85e091dc98a0dc09ea7f
Ben Penaluna <penalunaben@gmail.com>
2020-12-28 22:31:54 +1100

    Extended GeoFunctions.Core.Coordinates.Angle in order to allow for conversion to KML

---
+++ GoogleEarthConventions.Tests/KML/AbstractView/Attributes/HeadingTests.cs
@@ -0,0 +1,26 @@
+﻿using GeoFunctions.Core.Coordinates.Measurement;
+using GoogleEarthConversions.Core.KML.AbstractView.Attributes;
+using System;
+using Xunit;
+
+namespace GoogleEarthConventions.Tests.KML.AbstractView.Attributes
+{
+    public class HeadingTests
+    {
+        [Theory]
+        [InlineData(0, AngleMeasurement.Degrees, "<heading>0</heading>")]
+        [InlineData(72.999438826, AngleMeasurement.Degrees, "<heading>72.999438826</heading>")]
+        [InlineData(360, AngleMeasurement.Degrees, "<heading>0</heading>")]
+        [InlineData(450, AngleMeasurement.Degrees, "<heading>90</heading>")]
+        [InlineData(Math.PI, AngleMeasurement.Radians, "<heading>180</heading>")]
+        [InlineData(5.0*Math.PI/2.0, AngleMeasurement.Radians, "<heading>90</heading>")]
+        public void Heading_CorrectlyConvertsToKML(double heading, AngleMeasurement measurement, string expected)
+        {
+            var sut = new Heading(heading, measurement);
+
+            var result = sut.ConvertObjectToKML();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ GoogleEarthConversions/KML/AbstractView/Attributes/Heading.cs
@@ -0,0 +1,21 @@
+﻿using GeoFunctions.Core.Coordinates;
+using GeoFunctions.Core.Coordinates.Measurement;
+using GoogleEarthConversions.Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleEarthConversions.Core.KML.AbstractView.Attributes
+{
+    public class Heading : Angle, IKMLFormat
+    {
+        public Heading(double value = 0.0, AngleMeasurement measurement = AngleMeasurement.Degrees) : base(value, measurement) { }
+
+        public string ConvertObjectToKML()
+        {
+            var heading = AngleMeasurement == AngleMeasurement.Degrees ? CoTerminalValue : Angle.ToDegrees(CoTerminalValue);
+            
+            return string.Format("<{0}>{1}</{0}>", nameof(Heading).ConvertFirstCharacterToLowerCase(), heading);
+        }
+    }
+}
