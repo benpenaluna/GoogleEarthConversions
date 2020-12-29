6efcbe15c66ad3b517213ff18362963d057aaebd
Ben Penaluna <penalunaben@gmail.com>
2020-12-29 22:24:38 +1100

    Created a static generic method to test for equality when using == and !=

---
+++ GoogleEarthConversions/Common/EqualityCheck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleEarthConversions.Core.Common
+{
+    public static class EqualityCheck
+    {
+        public static bool ObjectEquals<T>(T a, T b)
+        {
+            if (a is null && b is null)
+            {
+                return true;
+            }
+
+            if (a is null || b is null)
+            {
+                return false;
+            }
+
+            return a.Equals(b);
+        }
+    }
+}
