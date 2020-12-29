a7db86d2b055a3512c0bab90d6a4f724f729f79f
Ben Penaluna <penalunaben@gmail.com>
2020-12-30 08:59:31 +1100

    Completed the Range class for use as a property in the LookAt class

---
+++ GoogleEarthConventions.Tests/KML/AbstractView/Attributes/RangeTests.cs
@@ -0,0 +1,50 @@
+﻿using GeoFunctions.Core.Coordinates;
+using GeoFunctions.Core.Coordinates.Measurement;
+using GoogleEarthConversions.Core.KML.AbstractView.Attributes;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GoogleEarthConventions.Tests.KML.AbstractView.Attributes
+{
+    public class RangeTests
+    {
+        [Theory]
+        [InlineData(0, DistanceMeasurement.Meters)]
+        public void Option_CanInstantiateWithIDistance(double distance, DistanceMeasurement measurement)
+        {
+            IDistance range = new Distance(distance, measurement); 
+            var sut = new Range(range);
+
+            Assert.NotNull(sut);
+        }
+
+        [Theory]
+        [InlineData(0, DistanceMeasurement.Meters)]
+        public void Option_AllPropertiesInitialisedWithIDistance(double distance, DistanceMeasurement measurement)
+        {
+            IDistance range = new Distance(distance, measurement);
+            var sut = new Range(range);
+
+            foreach (var prop in sut.GetType().GetProperties())
+            {
+                var value = prop.GetValue(sut);
+                Assert.NotNull(value);
+            }
+        }
+
+        [Theory]
+        [InlineData(0, DistanceMeasurement.Meters, "<range>0</range>")]
+        [InlineData(90, DistanceMeasurement.Meters, "<range>90</range>")]
+        [InlineData(9000, DistanceMeasurement.Centimeters, "<range>90</range>")]
+        [InlineData(5280, DistanceMeasurement.Feet, "<range>1609.344</range>")]
+        public void Altitude_CorrectlyConvertsToKML(double elevation, DistanceMeasurement measurement, string expected)
+        {
+            var sut = new Range(elevation, measurement);
+
+            var result = sut.ConvertObjectToKML();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ GoogleEarthConversions/KML/AbstractView/Attributes/Range.cs
@@ -0,0 +1,24 @@
+﻿using GeoFunctions.Core.Coordinates;
+using GeoFunctions.Core.Coordinates.Measurement;
+using GoogleEarthConversions.Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleEarthConversions.Core.KML.AbstractView.Attributes
+{
+    public class Range : Distance, IDistanceKML
+    {
+        public Range(double range = 0.0, DistanceMeasurement measurement = DistanceMeasurement.Feet) : base(range, measurement) { }
+        public Range(IDistance range)
+        {
+            Value = range.Value;
+            DistanceMeasurement = range.DistanceMeasurement;
+        }
+
+        public string ConvertObjectToKML()
+        {
+            return string.Format("<{0}>{1}</{0}>", nameof(Range).ConvertFirstCharacterToLowerCase(), ToMeters());
+        }
+    }
+}
