f73abe4a9f2f7763a45a5d711ba6e547db24ed96
Ben Penaluna <penalunaben@gmail.com>
2020-12-30 13:42:49 +1100

    Added MimAltitude and MaxAltitude for use in the LatLonAltBox class

---
+++ GoogleEarthConversions/KML/Object/Attributes/MaxAltitude.cs
@@ -0,0 +1,25 @@
+﻿using GeoFunctions.Core.Coordinates;
+using GeoFunctions.Core.Coordinates.Measurement;
+using GoogleEarthConversions.Core.Common;
+using GoogleEarthConversions.Core.Geographical;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleEarthConversions.Core.KML.Object.Attributes
+{
+    public class MaxAltitude : Distance, IDistanceKML
+    {
+        public MaxAltitude(double altitdue = 0.0, DistanceMeasurement measurement = DistanceMeasurement.Feet) : base(altitdue, measurement) { }
+        public MaxAltitude(IDistance altitude)
+        {
+            Value = altitude.Value;
+            DistanceMeasurement = altitude.DistanceMeasurement;
+        }
+
+        public string ConvertObjectToKML()
+        {
+            return string.Format("<{0}>{1}</{0}>", nameof(MaxAltitude).ConvertFirstCharacterToLowerCase(), ToMeters());
+        }
+    }
+}
