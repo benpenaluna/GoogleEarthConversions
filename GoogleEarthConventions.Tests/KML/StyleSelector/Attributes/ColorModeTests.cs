e612c2cfc117bb6e26d61551a625a8c8187c53aa
Ben Penaluna <penalunaben@gmail.com>
2021-01-03 22:02:42 +1100

    ColorMode class created for use in the ColorStyle abstract class

---
+++ GoogleEarthConventions.Tests/KML/StyleSelector/Attributes/ColorModeTests.cs
@@ -0,0 +1,43 @@
+﻿using GoogleEarthConversions.Core.KML.StyleSelector.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GoogleEarthConventions.Tests.KML.StyleSelector.Attributes
+{
+    public class ColorModeTests
+    {
+        [Fact]
+        public void ColorMode_CanInstantiate()
+        {
+            var sut = new ColorMode();
+
+            Assert.NotNull(sut);
+        }
+
+        [Fact]
+        public void ColorMode_AllPropertiesInitialised()
+        {
+            var sut = new ColorMode();
+
+            foreach (var prop in sut.GetType().GetProperties())
+            {
+                var value = prop.GetValue(sut);
+                Assert.NotNull(value);
+            }
+        }
+
+        [Theory]
+        [InlineData(ColorModeEnum.Normal, "<colorMode>normal</colorMode>")]
+        [InlineData(ColorModeEnum.Random, "<colorMode>random</colorMode>")]
+        public void LatLonAltBox_CorrectlyConvertsToKML(ColorModeEnum mode, string expected)
+        {
+            var sut = new ColorMode(mode);
+
+            var result = sut.ConvertObjectToKML();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ GoogleEarthConversions/KML/StyleSelector/Attributes/ColorMode.cs
@@ -0,0 +1,48 @@
+﻿using GoogleEarthConversions.Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleEarthConversions.Core.KML.StyleSelector.Attributes
+{
+    public class ColorMode : IColorMode
+    {
+        public ColorModeEnum Mode { get; set; }
+
+        public ColorMode(ColorModeEnum mode = ColorModeEnum.Normal)
+        {
+            Mode = mode;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj.GetType() == typeof(ColorMode) && Equals((ColorMode)obj);
+        }
+
+        protected bool Equals(ColorMode other)
+        {
+            return Equals(Mode, other.Mode);
+        }
+
+        public static bool operator ==(ColorMode a, ColorMode b)
+        {
+            return EqualityCheck.ObjectEquals(a, b);
+        }
+
+        public static bool operator !=(ColorMode a, ColorMode b)
+        {
+            return !EqualityCheck.ObjectEquals(a, b);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public string ConvertObjectToKML()
+        {
+            return string.Format("<{0}>{1}</{0}>", nameof(ColorMode).ConvertFirstCharacterToLowerCase(), 
+                                                   Mode.ToString().ConvertFirstCharacterToLowerCase());
+        }
+    }
+}
