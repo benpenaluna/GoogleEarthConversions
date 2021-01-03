0ff5494ff1feb7733d508c5d588936f93b9210bf
Ben Penaluna <penalunaben@gmail.com>
2021-01-03 16:39:01 +1100

    Created extension method to confirm that ExtendedData contains valid XML

---
+++ GoogleEarthConversions/Common/ValidityChecks.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace GoogleEarthConversions.Core.Common
+{
+    public static class ValidityChecks
+    {
+        public static bool IsValidXML(this string xml)
+        {
+            try
+            {
+                new XmlDocument().Load(xml);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
