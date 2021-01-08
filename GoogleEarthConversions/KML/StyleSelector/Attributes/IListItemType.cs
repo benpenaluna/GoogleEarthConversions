a2b2c543ea8d57e0d92b3f49b77111d716a02ec7
Ben Penaluna <penalunaben@gmail.com>
2021-01-08 19:10:42 +1100

    Should have been committed when commiting the ListItemType

---
+++ GoogleEarthConversions/KML/StyleSelector/Attributes/IListItemType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleEarthConversions.Core.KML.StyleSelector.Attributes
+{
+    public interface IListItemType : IKMLFormat
+    {
+        ListItemTypeEnum ItemType { get; set; }
+    }
+}
