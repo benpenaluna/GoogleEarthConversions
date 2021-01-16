8172e7faced48434f8a036c05803077135d703db
Ben Penaluna <penalunaben@gmail.com>
2021-01-16 16:05:37 +1100

    Completed StyleUrl for use in the StyleMap class

---
+++ GoogleEarthConventions.Tests/KML/StyleSelector/Attributes/StyleUrlTests.cs
@@ -0,0 +1,54 @@
+﻿using GoogleEarthConversions.Core.KML.StyleSelector.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GoogleEarthConventions.Tests.KML.StyleSelector.Attributes
+{
+    public class StyleUrlTests
+    {
+        [Theory]
+        [InlineData("https://user:password@www.contoso.com:80/Home/Index.htm?q1=v1&q2=v2#FragmentName")]
+        public void StyleUrl_CanInstantiate(string href)
+        {
+            var url = new Uri(href);
+            var sut = new StyleUrl(url);
+
+            Assert.NotNull(sut);
+        }
+
+        [Theory]
+        [InlineData("https://user:password@www.contoso.com:80/Home/Index.htm?q1=v1&q2=v2#FragmentName")]
+        public void StyleUrl_AllPropertiesInitialised(string href)
+        {
+            var url = new Uri(href);
+            var sut = new StyleUrl(url);
+
+            foreach (var prop in sut.GetType().GetProperties())
+            {
+                var value = prop.GetValue(sut);
+                Assert.NotNull(value);
+            }
+        }
+
+        [Fact]
+        public void StyleUrl_ThrowsExceptionOnInavlidUrl()
+        {
+            Assert.Throws<UriFormatException>(() => new StyleUrl(new Uri(string.Empty)));
+        }
+
+        [Theory]
+        [InlineData("https://user:password@www.contoso.com:80/Home/Index.htm?q1=v1&q2=v2#FragmentName", false, "<styleUrl>https://user:password@www.contoso.com:80/Home/Index.htm?q1=v1&q2=v2#FragmentName</styleUrl>")]
+        [InlineData("https://user:password@www.contoso.com:80/Home/Index.htm?q1=v1&q2=v2#FragmentName", true, "<styleUrl>#FragmentName</styleUrl>")]
+        public void StyleUrl_CorrectlyConvertsToKML(string href, bool styleLocal, string expected)
+        {
+            var uri = new Uri(href);
+            var sut = new StyleUrl(uri, styleLocal);
+
+            var result = sut.ConvertObjectToKML();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ GoogleEarthConversions/KML/StyleSelector/Attributes/StyleUrl.cs
@@ -0,0 +1,52 @@
+﻿using GoogleEarthConversions.Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleEarthConversions.Core.KML.StyleSelector.Attributes
+{
+    public class StyleUrl : IStyleUrl
+    {
+        public Uri Url { get; set; }
+
+        public bool StyleInLocalDocument { get; set; }
+
+        public StyleUrl(Uri href, bool styleLocal = true)
+        {
+            Url = href;
+            StyleInLocalDocument = styleLocal;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj.GetType() == typeof(StyleUrl) && Equals((StyleUrl)obj);
+        }
+
+        protected bool Equals(StyleUrl other)
+        {
+            return Equals(Url, other.Url);
+        }
+
+        public static bool operator ==(StyleUrl a, StyleUrl b)
+        {
+            return EqualityCheck.ObjectEquals(a, b);
+        }
+
+        public static bool operator !=(StyleUrl a, StyleUrl b)
+        {
+            return !EqualityCheck.ObjectEquals(a, b);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public string ConvertObjectToKML()
+        {
+            var url = StyleInLocalDocument ? Url.Fragment : Url.AbsoluteUri;
+            
+            return string.Format("<{0}>{1}</{0}>", nameof(StyleUrl).ConvertFirstCharacterToLowerCase(), url);
+        }
+    }
+}
