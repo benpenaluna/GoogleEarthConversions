61d00515ec4ef8348a25a27783dcf54e044af9dd
Ben Penaluna <penalunaben@gmail.com>
2021-01-17 22:07:57 +1100

    DummyTimePrimative created for use as the default TimePrimative in the Feature class

---
+++ GoogleEarthConversions/KML/TimePrimitive/DummyTimePrimitive.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleEarthConversions.Core.KML.TimePrimitive
+{
+    public class DummyTimePrimitive : TimePrimitive
+    {
+        public DummyTimePrimitive()
+        {
+            Id = string.Empty;
+            TargetId = string.Empty;
+        }
+        
+        public override string ConvertObjectToKML()
+        {
+            return string.Empty;
+        }
+    }
+}
